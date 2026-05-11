321fc3f37c4acab442c19031abf3d171bac7ac4e
Sanskruti Patil <psanskruti811@gmail.com>
2026-05-11 10:59:14 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from routers import resume, interview, summary
-
-app = FastAPI(title="AI Candidate Screening System")
-
-app.include_router(resume.router, prefix="/resume")
-app.include_router(interview.router, prefix="/interview")
-app.include_router(summary.router, prefix="/summary")
